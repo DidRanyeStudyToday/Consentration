555bf060485b7fe3bab7d332560b999bcd910115
openseem <asd657977201@gmail.com>
2021-05-25 11:34:42 +0800

    add calender

--- app/src/main/java/com/example/consentration/ui/clock/ClockFragment.kt
+++ app/src/main/java/com/example/consentration/ui/clock/ClockFragment.kt
@@ -9,23 +9,34 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.consentration.R
+import com.example.consentration.databinding.FragmentClockBinding
 
 class ClockFragment : Fragment() {
 
     private lateinit var clockViewModel: ClockViewModel
 
+    private lateinit var binding: FragmentClockBinding
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentClockBinding.inflate(inflater)
         clockViewModel =
-                ViewModelProvider(this).get(ClockViewModel::class.java)
+            ViewModelProvider(this).get(ClockViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_clock, container, false)
-        val textView: TextView = root.findViewById(R.id.text_clock)
-        clockViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
+        val monthView: TextView = root.findViewById(R.id.current_month)
+        clockViewModel.month.observe(viewLifecycleOwner, {
+            monthView.text = it
+        })
+
+        val dayView: TextView = root.findViewById(R.id.current_day)
+        clockViewModel.day.observe(viewLifecycleOwner, {
+            dayView.text = it
         })
         return root
     }
+
 }
--- app/src/main/java/com/example/consentration/ui/clock/ClockViewModel.kt
+++ app/src/main/java/com/example/consentration/ui/clock/ClockViewModel.kt
@@ -1,13 +1,39 @@
 package com.example.consentration.ui.clock
 
+import android.icu.util.LocaleData
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.LocalDateTime
+import java.time.Month
+import java.util.*
 
 class ClockViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _month = MutableLiveData<String>().apply {
+        val current = LocalDateTime.now()
+        value = when (current.month) {
+            Month.JANUARY -> "一月"
+            Month.FEBRUARY -> "二月"
+            Month.MARCH -> "三月"
+            Month.APRIL -> "四月"
+            Month.MAY -> "五月"
+            Month.JUNE -> "六月"
+            Month.JULY -> "七月"
+            Month.AUGUST -> "八月"
+            Month.SEPTEMBER -> "九月"
+            Month.OCTOBER -> "十月"
+            Month.NOVEMBER -> "十一月"
+            Month.DECEMBER -> "十二月"
+        }
+
     }
-    val text: LiveData<String> = _text
+
+    private val _day = MutableLiveData<String>().apply {
+        val current = LocalDateTime.now()
+        value = current.dayOfMonth.toString()
+    }
+
+    val month: LiveData<String> = _month
+    val day : LiveData<String> = _day
 }
