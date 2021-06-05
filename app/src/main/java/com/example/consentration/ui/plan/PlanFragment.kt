535865926976252c3f5be7146150bf88527f4762
DidRanyeStudyToday <2377210091@qq.com>
2021-06-06 03:18:34 +0800

    Plan模块初步完成，但是RecyclerView显示不出来，报错No adapter attached; skipping layout。查阅资料添加了layoutManager仍然无效，但不影响在此基础上开发。

--- app/src/main/java/com/example/consentration/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/example/consentration/ui/plan/PlanFragment.kt
@@ -4,41 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.consentration.R
-import com.example.consentration.databinding.FragmentClockBinding
 import com.example.consentration.databinding.FragmentPlanBinding
-import kotlin.let as let1
+import com.example.consentration.plan.Plan
+import com.example.consentration.plan.PlansAdapter
 
 class PlanFragment : Fragment() {
 
     private lateinit var planViewModel: PlanViewModel
-    private lateinit var binding: FragmentPlanBinding
 
-    private val data = listOf("Apple", "Banana", "Orange", "Watermelon",
-            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
-            "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape",
-            "Pineapple", "Strawberry", "Cherry", "Mango")
+    private lateinit var plansAdapter: PlansAdapter
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentPlanBinding.inflate(inflater)
         planViewModel =
                 ViewModelProvider(this).get(PlanViewModel::class.java)
-
         val root = inflater.inflate(R.layout.fragment_plan, container, false)
 
-        val todoListView: ListView = root.findViewById(R.id.todo_list)
-        val adapter = ArrayAdapter<String>(this.requireActivity(), android.R.layout.simple_list_item_1, data)
-        todoListView.adapter = adapter
+        val binding: FragmentPlanBinding = FragmentPlanBinding.inflate(inflater, container, false)
+
+        plansAdapter = PlansAdapter(mutableListOf())
+
+        binding.rvTodoItems.layoutManager = LinearLayoutManager(activity)
+        binding.rvTodoItems.adapter = plansAdapter
+
+        binding.btnAddTodo.setOnClickListener {
+            val todoTitle = binding.etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val plan = Plan(todoTitle,false)
+                plansAdapter.addTodo(plan)
+                binding.etTodoTitle.text.clear()
+            }
+        }
+        binding.btnDeleteDoneTodos.setOnClickListener {
+            plansAdapter.deleteDoneTodos()
+        }
+
+
         return root
     }
 }
