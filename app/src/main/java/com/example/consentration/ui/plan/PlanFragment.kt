fade7918a76a1db2866c39b4c24ce50527e5c309
DidRanyeStudyToday <2377210091@qq.com>
2021-06-07 01:51:51 +0800

    plan模块功能没有问题，但是每次重新加载fragment数据丢失，增加PlanViewModel.kt没有用处

--- app/src/main/java/com/example/consentration/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/example/consentration/ui/plan/PlanFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.consentration.R
 import com.example.consentration.databinding.FragmentPlanBinding
 import com.example.consentration.plan.Plan
 import com.example.consentration.plan.PlansAdapter
@@ -15,8 +13,7 @@ import com.example.consentration.plan.PlansAdapter
 class PlanFragment : Fragment() {
 
     private lateinit var planViewModel: PlanViewModel
-//    private lateinit var binding: FragmentPlanBinding
-    private lateinit var plansAdapter: PlansAdapter
+//    private lateinit var plansAdapter: PlansAdapter
 
     private var _binding: FragmentPlanBinding? = null
     private val  binding get() = _binding!!
@@ -26,29 +23,52 @@ class PlanFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-//        binding = FragmentPlanBinding.inflate(inflater, container, false)
-        _binding = FragmentPlanBinding.inflate(inflater, container, false)
+         _binding = FragmentPlanBinding.inflate(inflater, container, false)
         planViewModel =
                 ViewModelProvider(this).get(PlanViewModel::class.java)
 //        val root = inflater.inflate(R.layout.fragment_plan, container, false)
         val root = binding.root
 
-        plansAdapter = PlansAdapter(mutableListOf())
+
+//        plansAdapter = PlansAdapter(mutableListOf())
+
+//        var planList : MutableList<Plan>
+
+        planViewModel.planListLiveData.observe(viewLifecycleOwner, {
+            val plansAdapter = PlansAdapter(it)
+
+            binding.rvTodoItems.adapter = plansAdapter
+
+            binding.btnAddTodo.setOnClickListener {
+                val todoTitle = binding.etTodoTitle.text.toString()
+                if(todoTitle.isNotEmpty()) {
+                    val plan = Plan(todoTitle,false)
+                    plansAdapter.addTodo(plan)
+                    binding.etTodoTitle.text.clear()
+                }
+            }
+            binding.btnDeleteDoneTodos.setOnClickListener {
+                plansAdapter.deleteDoneTodos()
+            }
+//            planList = it
+        })
+
+//        plansAdapter = PlansAdapter(planList)
 
 //        binding.rvTodoItems.layoutManager = LinearLayoutManager(activity)
-        binding.rvTodoItems.adapter = plansAdapter
-
-        binding.btnAddTodo.setOnClickListener {
-            val todoTitle = binding.etTodoTitle.text.toString()
-            if(todoTitle.isNotEmpty()) {
-                val plan = Plan(todoTitle,false)
-                plansAdapter.addTodo(plan)
-                binding.etTodoTitle.text.clear()
-            }
-        }
-        binding.btnDeleteDoneTodos.setOnClickListener {
-            plansAdapter.deleteDoneTodos()
-        }
+//        binding.rvTodoItems.adapter = plansAdapter
+//
+//        binding.btnAddTodo.setOnClickListener {
+//            val todoTitle = binding.etTodoTitle.text.toString()
+//            if(todoTitle.isNotEmpty()) {
+//                val plan = Plan(todoTitle,false)
+//                plansAdapter.addTodo(plan)
+//                binding.etTodoTitle.text.clear()
+//            }
+//        }
+//        binding.btnDeleteDoneTodos.setOnClickListener {
+//            plansAdapter.deleteDoneTodos()
+//        }
 
 
         return root
--- app/src/main/java/com/example/consentration/ui/plan/PlanViewModel.kt
+++ app/src/main/java/com/example/consentration/ui/plan/PlanViewModel.kt
@@ -3,7 +3,34 @@ package com.example.consentration.ui.plan
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.consentration.plan.Plan
+import com.example.consentration.plan.PlansAdapter
+import com.example.consentration.plan.planList
+import java.time.LocalDate
 
 class PlanViewModel : ViewModel() {
+//    private val _planListLiveData = MutableLiveData<List<Plan> >().apply {
+//        value = mutableListOf()
+//    }
+//
+//    val planListLiveData: LiveData<List<Plan> > = _planListLiveData
+
+//    private val _plansAdapter = MutableLiveData<PlansAdapter>().apply{
+//        value = PlansAdapter(mutableListOf())
+//    }
+//
+//    val plansAdapter: LiveData<PlansAdapter> get() = _plansAdapter
+
+//    val planListLiveData : MutableLiveData< MutableList<Plan> > = MutableLiveData(mutableListOf())
+
+    private var _planListLiveData = MutableLiveData< MutableList<Plan> >()
+
+    init {
+        _planListLiveData.value = mutableListOf()
+    }
+
+    val planListLiveData: LiveData<MutableList<Plan>> get() = _planListLiveData
+
+
 
 }
